57dee9cd11c9afe0dfbc0ccefbe47f54e40cb09c
Nayan Buha <nayanbuha900@gmail.com>
2021-09-08 10:23:30 +0530

    String methods concate charAt  equals.

---
+++ String Methods/src/StringMethods/Demo/Demo.java
@@ -0,0 +1,16 @@
+package StringMethods.Demo;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		
+		String str="Nayan";
+		String str1="Buha";
+		Integer a=new Integer(5);
+		Integer b= new Integer(5);
+		
+		System.out.println("Concate Methods :" +str.concat(str1));
+		System.out.println("CharAt Mehods :" +str.charAt(4));
+		System.out.println("equals Mehods :" +a.equals(b));
+	}
+}
