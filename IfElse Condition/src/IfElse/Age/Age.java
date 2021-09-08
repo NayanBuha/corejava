ff6f6ccaaa772eb96b071db178b9303f1c9eb65d
Nayan Buha <nayanbuha900@gmail.com>
2021-09-08 10:16:13 +0530

    Added IfElse

---
+++ IfElse Condition/src/IfElse/Age/Age.java
@@ -0,0 +1,14 @@
+package IfElse.Age;
+
+public class Age {
+
+	public static void main(String[] args) {
+
+		int age = 20;
+		if (age >= 20) {
+			System.out.println("Your Age is : " + age);
+		} else {
+			System.out.println("This is not my Age");
+		}
+	}
+}
