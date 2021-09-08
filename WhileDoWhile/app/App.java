5af120d2879a449d07fad2e2ac5627d0a8d8759b
Nayan Buha <nayanbuha900@gmail.com>
2021-09-08 10:52:10 +0530

    Add WhileDoWhile Loop

---
+++ WhileDoWhile/app/App.java
@@ -0,0 +1,16 @@
+package app;
+
+public class App {
+
+	public static void main(String[] args) {
+
+		int i = 1;
+		/*
+		 * while(i<=10) { System.out.println(i); i++; }
+		 */
+		do {
+			System.out.println(i);
+			i++;
+		} while (i <= 10);
+	}
+}
