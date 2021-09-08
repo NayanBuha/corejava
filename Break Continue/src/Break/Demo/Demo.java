db8d20e921dd92b5441b2470d3416eae56cb39f5
Nayan Buha <nayanbuha900@gmail.com>
2021-09-08 10:53:08 +0530

    Break and Continue

---
+++ Break Continue/src/Break/Demo/Demo.java
@@ -0,0 +1,14 @@
+package Break.Demo;
+
+public class Demo {
+
+	public static void main(String[] args) {
+
+		for (int i = 1; i <= 10; i++) {
+			if (i == 7) {
+				continue;
+			}
+			System.out.println(i);
+		}
+	}
+}
