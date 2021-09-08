d1a614d4cb49546c933418db3c9f11cb6a44e2b9
Nayan Buha <nayanbuha900@gmail.com>
2021-09-08 10:13:30 +0530

    Added For Loop

---
+++ For_Loop/src/ForLoop/demp/Demo.java
@@ -0,0 +1,16 @@
+package ForLoop.demp;
+
+public class Demo {
+
+	public static void main(String[] args) {
+
+		int i, j;
+
+		for (i = 0; i <= 5; i++) {
+			for (j = 1; j <= i; j++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+	}
+}
